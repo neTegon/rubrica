b1ffb4bbe0d67fa956eff7f2d907ffdeb287d735
Elettra Tegon <112578860+neTegon@users.noreply.github.com>
2024-01-31 10:31:58 +0100

    Add files via upload

---
+++ tegon.elettra.4i.rubricaWPF/Contatto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tegon.elettra._4i.rubricaWPF
+{
+    public enum TipoContatto { nessuno, Email, web,Telefono }
+    public class Contatto
+    {
+        public int idPersona {  get; set; }
+        public string valore { get; set; }
+        public TipoContatto tipo { get; set; }
+
+        public Contatto() { }
+        
+        public Contatto(string riga)
+        {
+            int id;
+            TipoContatto c;
+            string[] campi = riga.Split(',');
+            int.TryParse(campi[0], out id);
+            Enum.TryParse(campi[1], out c);
+            tipo = c;
+            idPersona = id;
+            this.valore= campi[2];
+        }
+    }
+}
---
+++ tegon.elettra.4i.rubricaWPF/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tegon.elettra._4i.rubricaWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loader(object sender, RoutedEventArgs e)
+        {
+
+            
+            int idx = 0;
+            try
+            {
+
+                List<Contatto> contattos = new List<Contatto>();
+                List<Persone> persones = new List<Persone>();
+                //file conttatti
+                StreamReader sr = new("Contatti.csv");
+                sr.ReadLine();
+                string riga =string.Empty;
+
+                while (!sr.EndOfStream)
+                {
+                    riga = sr.ReadLine();
+                    contattos.Add(new Contatto(riga));
+                    idx++;
+                }
+                idx = 0;
+                dgContatti.ItemsSource = contattos;
+
+                StreamReader pers = new("Persona.csv");
+                pers.ReadLine();
+                string rigaPers = string.Empty;
+
+                while (!pers.EndOfStream)
+                { 
+                    rigaPers = sr.ReadLine();
+                    persones.Add(new Persone(rigaPers));
+                    idx++;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}\nErrore alla riga: {idx}!");
+            }
+
+        }
+
+        private void dgDati_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+    }
+}
