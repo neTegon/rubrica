e946194992cb4ab811bae8ea43abbb10e01fb97d
Elettra Tegon <112578860+neTegon@users.noreply.github.com>
2024-02-15 20:12:04 +0100

    Add files via upload

---
+++ tegon.elettra.4i.rubricaWPF/Contatto.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tegon.elettra._4i.rubricaWPF
+{
+    public enum TipoContatto { nessuno, Email, web,Telefono, Instagram,Facebook }
+    
+    public class Contatto
+    {
+        public int idPersona {  get; set; }
+        public string valore { get; set; }
+        public TipoContatto tipo { get; set; }
+        public int Numero {
+            get
+            {
+                return _numero;
+            }
+
+            set
+            {
+                if (value < 0 || value > 100)
+                    throw new ArgumentOutOfRangeException();
+
+                _numero = value;
+            }
+        }
+
+        private int _numero;
+
+        public Contatto()
+        {
+            tipo = TipoContatto.nessuno;
+        }
+
+        public Contatto(string riga)
+        {
+            int id;
+            TipoContatto c;
+            string[] campi = riga.Split(';');
+            if (campi.Count() != 3)
+            {
+                throw new Exception("Le righe devono essere di tre colonne");
+            }
+            int.TryParse(campi[0], out id);
+            Enum.TryParse(campi[1], out c);
+            tipo = c;
+            idPersona = id;
+            this.valore= campi[2];
+        }
+        public static Contatto MakeContatto(string riga)
+        {
+            
+            string[] campi = riga.Split(';');
+
+            TipoContatto c;
+            Enum.TryParse(campi[1], out c);
+
+            switch (c)
+            {
+                case TipoContatto.Email:
+                    return new CEmail(riga);
+                    
+
+                case TipoContatto.Telefono:
+                    return new CNumero(riga);
+                   
+
+                case TipoContatto.CWeb:
+                    return new CWeb(riga);
+                   
+
+                case TipoContatto.Instagram:
+                    return new CInstagram(riga);
+                    
+
+                case TipoContatto.Facebook:
+                    return new CFacebook(riga);
+                   
+
+            }
+            return new Contatto(riga);
+        }
+    }
+    public class Contatti : List<Contatto>
+    {
+        public Contatti() { }
+
+        public Contatti(string nomeFile)
+        {
+            StreamReader ts = new StreamReader("Contatti.csv");
+            ts.ReadLine();
+
+            while (!ts.EndOfStream)
+            {
+                base.Add(Contatto.MakeContatto(ts.ReadLine()));
+
+                //dgContatti.ItemsSource = Contatti;
+            }
+        }
+
+    }
+    public class CEmail : Contatto
+    {
+        public CEmail() { }
+
+        public CEmail(string riga)
+            : base(riga)
+        { }
+
+    }
+
+    public class CNumero : Contatto
+    {
+        public CNumero() { }
+
+        public CNumero(string riga)
+            : base(riga)
+        {
+
+        }
+    }
+    public class CWeb : Contatto
+    {
+        public CWeb() { }
+
+        public CWeb(string riga)
+            : base(riga)
+        {
+
+        }
+    }
+
+    public class CInstagram : Contatto
+    {
+        public CInstagram() { }
+
+        public CInstagram(string riga)
+            : base(riga)
+        {
+
+        }
+    }
+
+    public class CFacebook : Contatto
+    {
+        public CFacebook() { }
+
+        public CFacebook(string riga)
+            : base(riga)
+        {
+
+        }
+
+    }
+}
---
+++ tegon.elettra.4i.rubricaWPF/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tegon.elettra._4i.rubricaWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Persone person = new Persone();
+        private void Window_Loader(object sender, RoutedEventArgs e)
+        {
+
+
+            int idx = 0;
+            try
+            {
+
+                List<Contatto> contattos = new List<Contatto>();
+                List<Persona> persones = new List<Persona>();
+                //file conttatti
+                StreamReader sr = new("Contatti.csv");
+                sr.ReadLine();
+                string riga = string.Empty;
+
+                while (!sr.EndOfStream)
+                {
+                    riga = sr.ReadLine();
+                    contattos.Add(new Contatto(riga));
+                    idx++;
+                }
+                idx = 0;
+                dgContatti.ItemsSource = contattos;
+
+                StreamReader pers = new("Persona.csv");
+                pers.ReadLine();
+                string rigaPers = string.Empty;
+
+                while (!pers.EndOfStream)
+                {
+                    rigaPers = sr.ReadLine();
+                    persones.Add(new Persona(rigaPers));
+                    idx++;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}\nErrore alla riga: {idx}!");
+            }
+
+        }
+
+        private void dgDati_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Persona p = e.AddedItems[0] as Persona;
+            StatusBar.Text = $"{p.cognome}";
+
+            List<Contatto> ContattiFiiltrati = new List<Contatto>();
+            foreach (var item in Contatto)
+            {
+                if (item.idPersona == p.idPersona)
+                {
+                    ContattiFiiltrati.Add(item);
+
+                    dgContatti.ItemsSource = ContattiFiiltrati;
+
+                }
+            }
+        }
+    }
+}
